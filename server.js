cfba6e47d49682d28f4aa093bfc97e1b65cff8f2
Nell <eemandersson@outlook.com>
2023-05-21 19:12:43 +0200

    styling

--- server.js
+++ server.js
@@ -22,25 +22,65 @@ app.get("/members/create", (req, res) => {
 });
 
 app.post("/members/create", async (req, res) => {
-	await membersCollection.insertOne(req.body);
+	const dateAdded = new Date();
+	await membersCollection.insertOne({ ...req.body, date: dateAdded });
 	res.redirect("/members");
 });
 
 app.get("/members", async (req, res) => {
-	const members = await membersCollection.find().toArray();
+	const sort = req.query.sort;
+	let result = membersCollection.find();
+	if (sort === "ascending") {
+		result = result.sort({ name: 1 });
+	} else if (sort === "decending") {
+		result = result.sort({ name: -1 });
+	}
+	const members = await result.toArray();
 	res.render("members", { members });
 });
 
 app.get("/member/:id", async (req, res) => {
 	const temp = new ObjectId(req.params.id);
 	const member = await membersCollection.findOne({ _id: temp });
+
+	const dateString = member.date.toLocaleDateString("sv-SE");
+
 	res.render("member", {
+		name: member.name,
+		email: member.email,
+		phoneNumber: member.phoneNumber,
+		date: dateString,
+		dogName: member.dogName,
+		id: member._id,
+	});
+});
+
+app.get("/member/edit/:id", async (req, res) => {
+	console.log("HEJ");
+	const temp = new ObjectId(req.params.id);
+	const member = await membersCollection.findOne({ _id: temp });
+	res.render("edit", {
 		name: member.name,
 		email: member.email,
 		phoneNumber: member.phoneNumber,
 		date: member.date,
 		dogName: member.dogName,
+		id: member._id,
 	});
+});
+
+app.get("/member/delete/:id", async (req, res) => {
+	const temp = new ObjectId(req.params.id);
+	await membersCollection.deleteOne({
+		_id: temp,
+	});
+	res.redirect("/members");
+});
+
+app.post("/member/edit/:id", async (req, res) => {
+	const temp = new ObjectId(req.params.id);
+	await membersCollection.updateOne({ _id: temp }, { $set: req.body });
+	res.redirect("/members");
 });
 
 app.listen(3000, () => {
